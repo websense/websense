580a7eb97913fb1b23b9d4770540c8cb686ee515
limade <lisamay_42@yahoo.de>
2012-08-20 18:50:18 +0200

    Help Documentation with Online Help and PDF file

---
+++ Help_Documentation/English/content/search/htmlFileList.js
@@ -0,0 +1,23 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "FAQ.html";
+fl["1"]= "ch01s02.html";
+fl["2"]= "ch02s02.html";
+fl["3"]= "Create.Graph.html";
+fl["4"]= "ch02s01.html";
+fl["5"]= "ch03s05.html";
+fl["6"]= "Create.Heatmap.html";
+fl["7"]= "ch03s04.html";
+fl["8"]= "ch01s03.html";
+fl["9"]= "ch03s03.html";
+fl["10"]= "Anleitungen.html";
+fl["11"]= "UserInterface.html";
+fl["12"]= "index.html";
+fl["13"]= "Menü2.html";
+fl["14"]= "ch01s04.html";
+fl["15"]= "Create.CSV.html";
+fl["16"]= "ch03s07.html";
+fl["17"]= "Problemlösung.html";
+fl["18"]= "ch01s01.html";
+fl["19"]= "Introduction.html";
+var doStem = true
